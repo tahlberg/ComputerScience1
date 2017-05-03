0c0cc38b5e2e0ac5351b4fec7b06350517a39c44
tahlberg <trevorjahlberg@gmail.com>
2017-05-02 17:41:02 -0700

    Update main.cpp

--- HonorsProject/Sources/main.cpp
+++ HonorsProject/Sources/main.cpp
@@ -7,6 +7,51 @@ int main()
     GraphicsWindow gameWindow(1024, 768, "CS Honors");
     Game game;
     game = StartGame(game);
-    cout << game.player.totalHealth << endl;
-    MenuGraphic(gameWindow, game);
+    int buttonChoice;
+    int numPrint = 0;
+    while(game.player.alive == true)
+    {
+        while(game.boss.alive == true)
+        {
+            MenuGraphic(gameWindow, game);
+            buttonChoice = ButtonMenu(gameWindow);
+            switch(buttonChoice)
+            {
+            case 1:
+                {
+                    game = SlashAttack(game);
+                    FeedSlashAttack(gameWindow, numPrint);
+                    break;
+                }
+            case 2:
+                {
+                    game = StabAttack(game);
+                    FeedStabAttack(gameWindow, numPrint);
+                }
+                break;
+            case 3:
+                {
+                    game = SmashAttack(game);
+                    FeedSmashAttack(gameWindow, numPrint);
+                }
+                break;
+            case 4:
+                {
+                    game = ParryAttack(game);
+                    FeedParryAttack(gameWindow, numPrint);
+                }
+                break;
+            }
+            game = ResolvePDamage(game);
+            FeedPDamage(gameWindow, numPrint, game);
+            game = ResolveBDamage(game);
+            FeedBDamage(gameWindow, numPrint, game);
+            if(game.player.alive == false)
+            {
+                break;
+            }
+        }
+        game = GenNewBoss(game);
+    }
+    DeathScreen(gameWindow, game);
 }
